3925137a641fd622e7e2042bdf93ac9de6ddab65
Serg Ryabinin <scanner85@gmail.com>
2022-07-30 22:35:47 +0300

    4.2.7

---
+++ universal/src/main/java/ru/odybo/universal/CarDriver.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ru.odybo.universal;
+
+
+/**
+ *
+ * @author vragos
+ */
+public class CarDriver {
+
+    public static final class Car implements AutoCloseable{
+
+        @Override
+        public void close() {
+            try {
+                System.out.println("Машина закрывается...");
+            } catch (RuntimeException re) {
+                // need press this exception    
+                // System.out.println("runtime exception");
+            }
+        }
+
+        public void drive() {
+            System.out.println("Машина поехала.");
+        }
+        
+        public void makeNullPointer() {
+            Object o = null;
+            o.equals(0);//NullPointerException
+        }
+    }
+
+    public static void main(String[] args) {
+        try( Car car = new Car()) {
+            car.drive();
+            car.makeNullPointer();
+        } catch (Exception e) {
+            // System.out.println("runtime exception");
+        }
+    }
+}
+
